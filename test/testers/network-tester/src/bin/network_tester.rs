423eef82ab22fc7a4569e9fc3c8eeea631d71328
lamafab <42901763+lamafab@users.noreply.github.com>
2019-10-16 18:29:57 +0200

    sample authentication and multiplex construct, todo

--- test/testers/network-tester/src/bin/network_tester.rs
+++ test/testers/network-tester/src/bin/network_tester.rs
@@ -5,19 +5,25 @@ use libp2p::tcp::TcpConfig;
 use libp2p::identity::Keypair;
 use libp2p::mplex::MplexConfig;
 use libp2p::core::upgrade::SelectUpgrade;
+use libp2p::core::nodes::network::Network;
 use libp2p::core::transport::upgrade::{Builder, Version};
 use libp2p::yamux;
+//use libp2p_secio as secio;
+use libp2p::Swarm;
+use libp2p::secio::SecioConfig;
+use libp2p::swarm::protocols_handler::{ProtocolsHandler, NodeHandlerWrapper, SubstreamProtocol, DummyProtocolsHandler};
+use libp2p::swarm::toggle::Toggle;
 
 fn main() {
     env_logger::init();
 
-    // Create a random PeerId.
-    let id_keys = Keypair::generate_ed25519();
-    let peer_id = PeerId::from(id_keys.public());
-    println!("Local peer id: {:?}", peer_id);
+    let local_key = Keypair::generate_ed25519();
+    let local_public_key = local_key.public();
 
-    let transport = TcpConfig::new();
-    let builder = transport.upgrade(Version::V1);
-    let res = builder.multiplex(MplexConfig::new());
+    let transport = TcpConfig::new()
+        .upgrade(Version::V1)
+        .authenticate(SecioConfig::new(local_key))
+        .multiplex(MplexConfig::new());
 
+    //let swarm = Swarm::new(transport, DummyProtocolsHandler::default(), local_public_key);
 }
