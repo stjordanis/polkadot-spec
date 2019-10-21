fecac0570b03335d6a625f6bb20fc008b00a95d5
lamafab <42901763+lamafab@users.noreply.github.com>
2019-10-21 13:42:48 +0200

    removed commented code and improved log statements

--- test/testers/network-tester/src/bin/network_tester.rs
+++ test/testers/network-tester/src/bin/network_tester.rs
@@ -24,20 +24,16 @@ use tokio::io::{AsyncRead, AsyncWrite};
 use libp2p::kad::{Kademlia, KademliaEvent};
 use libp2p::mdns::service::{MdnsPacket, MdnsService};
 use libp2p::kad::record::store::MemoryStore;
+use libp2p::identify::{Identify, IdentifyEvent};
 
 fn main() {
     env_logger::init();
-
-    let local_key = Keypair::generate_ed25519();
-    let local_public_key = local_key.public();
-    let peer_id = PeerId::from_public_key(local_public_key);
-
-    println!("ID: {:?}", peer_id);
 
     #[derive(NetworkBehaviour)]
     struct MyBehaviour<TSubstream: AsyncRead + AsyncWrite> {
         kademlia: Kademlia<TSubstream, MemoryStore>,
         mdns: Mdns<TSubstream>,
+        identify: Identify<TSubstream>,
     }
 
     impl<TSubstream: AsyncRead + AsyncWrite> NetworkBehaviourEventProcess<MdnsEvent> for MyBehaviour<TSubstream> {
@@ -45,31 +41,47 @@ fn main() {
             match event {
                 MdnsEvent::Discovered(addrs) => {
                     for (peer_id, multi_addr) in addrs {
-                        info!("Discovered {}/{}", &multi_addr, peer_id);
+                        debug!("Discovered {}/{}", &multi_addr, peer_id);
                         self.kademlia.add_address(&peer_id, multi_addr);
                     }
 
                 },
-                _ => {},
+                e @ _ => {
+                    warn!("Received unhandled mDNS event: {:?}", e);
+                },
             }
         }
     }
 
     impl<TSubstream: AsyncRead + AsyncWrite> NetworkBehaviourEventProcess<KademliaEvent> for MyBehaviour<TSubstream> {
-        // Called when `floodsub` produces an event.
         fn inject_event(&mut self, event: KademliaEvent) {
             match event {
                 KademliaEvent::RoutingUpdated{peer, addresses, ..} => {
-                    info!("Updated DHT with {} - {:?}", peer, addresses);
+                    info!("Updated DHT with address of {}:", peer);
+                    for address in addresses.iter() {
+                        info!("> {}", address);
+                    }
                 },
-                _ => {},
+                e @ _ => {
+                    warn!("Received unhandled Kademlia event: {:?}", e);
+                },
             }
         }
     }
 
-    // remote node
-    let remote_peer_id: PeerId = "Qmd6oSuC4tEXHxewrZNdwpVhn8b4NwxpboBCH4kHkH1EYb".parse().unwrap();
-    let remote_addr: Multiaddr = "/ip4/127.0.0.1/tcp/30333".parse().unwrap();
+    impl<TSubstream: AsyncRead + AsyncWrite> NetworkBehaviourEventProcess<IdentifyEvent> for MyBehaviour<TSubstream> {
+        fn inject_event(&mut self, event: IdentifyEvent) {
+            match event {
+                _ => info!("IDEN: {:?}", event),
+            }
+        }
+    }
+
+    let local_key = Keypair::generate_ed25519();
+    let local_public_key = local_key.public();
+    let peer_id = PeerId::from_public_key(local_public_key.clone());
+
+    println!("ID: {:?}", peer_id);
 
     let transport = TcpConfig::new()
         .upgrade(Version::V1)
@@ -84,13 +96,19 @@ fn main() {
     let behaviour = MyBehaviour {
         kademlia: behaviour,
         mdns: mdns,
+        identify: Identify::new(String::from("/substrate/1.0"), String::from(""), local_public_key.clone()),
     };
 
+    // remote node
+    let remote_peer_id: PeerId = "Qmd6oSuC4tEXHxewrZNdwpVhn8b4NwxpboBCH4kHkH1EYb".parse().unwrap();
+    let remote_addr: Multiaddr = "/ip4/127.0.0.1/tcp/30333".parse().unwrap();
+
+    // Create swarm, listen on local port
     let mut swarm = Swarm::new(transport.clone(), behaviour, peer_id);
     let _ = Swarm::dial_addr(&mut swarm, remote_addr.clone()).unwrap();
-
     Swarm::listen_on(&mut swarm, "/ip4/0.0.0.0/tcp/0".parse().unwrap()).unwrap();
 
+    // Run worker
     let mut listening = false;
     tokio::run(futures::future::poll_fn(move || -> Result<_, ()> {
         loop {
@@ -112,66 +130,4 @@ fn main() {
 
         Ok(Async::NotReady)
     }));
-
-    // Kick it off
-    /*
-    tokio::run(futures::future::poll_fn(move || -> Result<_, ()> {
-        let text = "some test data";
-        loop {
-            let packet = match service.poll() {
-                Async::Ready(packet) => packet,
-                Async::NotReady => return Ok(Async::NotReady),
-            };
-
-            match packet {
-                MdnsPacket::Query(query) => {
-                    // We detected a libp2p mDNS query on the network. In a real application, you
-                    // probably want to answer this query by doing `query.respond(...)`.
-                    println!("Detected query from {:?}", query.remote_addr());
-                }
-                MdnsPacket::Response(response) => {
-                    // We detected a libp2p mDNS response on the network. Responses are for
-                    // everyone and not just for the requester, which makes it possible to
-                    // passively listen.
-                    for peer in response.discovered_peers() {
-                        println!("Discovered peer {:?}", peer.id());
-                        // These are the self-reported addresses of the peer we just discovered.
-                        for addr in peer.addresses() {
-                            println!(" Address = {:?}", addr);
-                        }
-                    }
-                }
-                MdnsPacket::ServiceDiscovery(query) => {
-                    // The last possibility is a service detection query from DNS-SD.
-                    // Just like `Query`, in a real application you probably want to call
-                    // `query.respond`.
-                    println!("Detected service query from {:?}", query.remote_addr());
-                }
-            }
-
-            /*
-            match conn.poll().unwrap() {
-                Async::Ready(mut stream) => {
-                    match stream.poll_write(text.as_bytes()).unwrap() {
-                        Async::Ready(x) => {
-                            println!("{:?}", x);
-                        },
-                        Async::NotReady => {
-                            println!("Leaving stream");
-                            break;
-                        }
-                    }
-                },
-                Async::NotReady => {
-                    println!("Leaving connection");
-                    break;
-                },
-            }
-            break;
-            */
-        }
-
-        Ok(Async::NotReady)
-    }));
-    */
 }
